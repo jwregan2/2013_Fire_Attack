0e24a10c2c7003c84c41ea722caa8ff78269da5a
Robin Zevotek <Robin.Zevotek@ul.com>
2017-05-19 17:10:02 -0400

    work on flow and move vs. shutdown and move

--- Part_II/1_Scripts/Tatic_Considerations_Plot.py
+++ Part_II/1_Scripts/Tatic_Considerations_Plot.py
@@ -39,15 +39,17 @@ print ('---------------------------------Plot Flow & Move vs. Shutdown and Move 
 
 experiments = {'No':['Experiment_2_Data', 'Experiment_6_Data'], 'Single':['Experiment_7_Data', 'Experiment_8_Data'], 'Two':['Experiment_13_Data','Experiment_14_Data']}
 
-experiment = ['Experiment_3_Data', 'Experiment_2_Data', 'Experiment_6_Data','Experiment_7_Data', 'Experiment_8_Data', 'Experiment_13_Data','Experiment_14_Data']
+experiment = ['Experiment_3_Data', 'Experiment_2_Data', 'Experiment_6_Data','Experiment_7_Data', 'Experiment_8_Data', 'Experiment_9_Data', 'Experiment_10_Data', 'Experiment_13_Data','Experiment_14_Data']
 
 channels = {'End_Hall':['3TC7','3TC6','3TC5','3TC4','3TC3','3TC2','3TC1'], 
-			'Mid_Hall':['4TC1','4TC2','4TC3','4TC4','4TC5','4TC6','4TC7'],
-			'Start_Hall':['5TC1','5TC2','5TC3','5TC4','5TC5','5TC6','5TC7'],
-			'Living_Room':['6TC1','6TC2','6TC3','6TC4','6TC5','6TC6','6TC7'],
-			'Bedroom_3':['8TC1','8TC3','8TC5','8TC7'],
+			# 'Mid_Hall':['4TC1','4TC2','4TC3','4TC4','4TC5','4TC6','4TC7'],
+			# 'Start_Hall':['5TC1','5TC2','5TC3','5TC4','5TC5','5TC6','5TC7'],
+			# 'Living_Room':['6TC1','6TC2','6TC3','6TC4','6TC5','6TC6','6TC7'],
+			# 'Bedroom_3':['8TC1','8TC3','8TC5','8TC7'],
 			'Bedroom_1':['1TC1', '1TC3', '1TC5', '1TC7'],
-			'Bedroom_2':['2TC1','2TC3','2TC5', '2TC7']}
+			# 'Bedroom_2':['2TC1','2TC3','2TC5', '2TC7']
+			'Start_Hall_Flow':['6BDP1','6BDP2','6BDP3','6BDP4','6BDP5']
+			}
 
 # experiment = all_exp_data.keys()
 
@@ -91,7 +93,6 @@ for exp in experiment:
 		plt.xlabel('Time (min)', fontsize=48)
 		plt.xticks(fontsize=44)
 		plt.yticks(fontsize=44)
-		plt.ylabel('Temperature ($^\circ$F)', fontsize = 48)
 
 		ax2 = ax1.twinx()
 		ax2.plot(all_flow_data[exp].index.values/60, all_flow_data[exp]['Total Gallons'], lw=6, color='#1f77b4',)
@@ -109,35 +110,47 @@ for exp in experiment:
 		for channel in sorted(channels[sensor], reverse=True):
 			if channel in channels_to_skip[exp]:
 				continue
+
+			if 'BDP' in channel:
+				channel_name = channel + 'V'
+			else:
+				channel_name = channel
+
 			ax1.plot(all_exp_data[exp][channel].index/60, all_exp_data[exp][channel], lw = 4, marker=next(plot_markers), markevery=mark,
-						label = all_channels['Title'][channel])
-		
+						label = all_channels['Title'][channel_name])
+
 		flow_data = all_flow_data[exp]['GPM'] 
 
+		fill = 100
+
 		if 'HF' in channel:
-			fill = 5
+			fill_min = 0
+			fill_max = float(Exp_Des['Y Scale HF'][exp])
+			y_title = 'Heat Flux (kW/m$^2$'
+		elif 'BDP' in channel:
+			fill_min = float(-Exp_Des['Y Scale BDP'][exp])
+			fill_max = float(Exp_Des['Y Scale BDP'][exp])
+			y_title = 'Velocity (m/s)'
 		else:
-			fill = Exp_Des['Y Scale Temperature'][exp]	
-		
-		
+			fill_min = 0
+			fill_max = float(Exp_Des['Y Scale Temperature'][exp])
+			y_title = 'Temperature ($^\circ$F)'	
 
 		if 'Burst Suppression' in all_exp_events[exp[:-4]+ 'Events'].index.values:
-			if exp == 'Experiment_14_Data':
+			if exp in ['Experiment_8_Data', 'Experiment_14_Data']:
 				burst = -0.1
 			else:
 				burst = 0
 			ax1.axvline(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Burst Suppression']/60, lw = 4, color='black')
-			ax1.text(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Burst Suppression']/60+burst, Exp_Des['Y Scale Temperature'][exp], 'Burst Suppression',
+			ax1.text(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Burst Suppression']/60+burst, fill_max, 'Burst Suppression',
 				ha='left', va='bottom', rotation=45, fontsize=34)
 		
 		if 'Hall Suppression' in all_exp_events[exp[:-4]+ 'Events'].index.values:
 			ax1.axvline(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Hall Suppression']/60, lw = 4, color='black')
-			ax1.text(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Hall Suppression']/60, Exp_Des['Y Scale Temperature'][exp], 'Hall Suppression', 
+			ax1.text(all_exp_events[exp[:-4] + 'Events']['Time_Seconds']['Hall Suppression']/60, fill_max, 'Hall Suppression', 
 				ha='left', va='bottom', rotation=45, fontsize=34)
 
-		ax1.fill_between(flow_data.index.values/60, 0,  fill, where =  flow_data > 10, facecolor='blue', alpha=0.3)
-
-
+		ax1.fill_between(flow_data.index.values/60, fill_min,  fill_max, where =  flow_data > 10, facecolor='blue', alpha=0.1)
 
 		plt.xlim([all_exp_events[exp[:-4] + 'Events']['Time_Seconds'][1]/60, 
 		# plt.xlim([0, 
@@ -152,6 +165,8 @@ for exp in experiment:
 		h1, l1 = ax1.get_legend_handles_labels()
 		h2, l2 = ax2.get_legend_handles_labels()
 		ax1.legend(h1+h2, l1+l2, loc='upper right', fontsize=40, handlelength=3, labelspacing=.15 )
+		ax1.set_ylim(fill_min,fill_max)
+		ax1.set_ylabel(y_title, fontsize = 48)
 
 		if not os.path.exists(output_location +  exp[:-5] + '/'):
 			os.makedirs(output_location +  exp[:-5] + '/')
@@ -159,3 +174,7 @@ for exp in experiment:
 		plt.subplots_adjust(top=0.80)
 		plt.savefig(output_location +  exp[:-5] + '/' + sensor +'.pdf')
 		plt.close('all')
+
+# print ('---------------------------------Plotting Experiment Comparison Plots -----------------------------')
+
+
