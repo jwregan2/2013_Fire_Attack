7244aa2a32cdb2d9273d24fda6c5a9a0ce466eb9
Joe Willi <willijm92@gmail.com>
2017-06-05 13:24:43 -0400

    Adjusted plots for 3rd sampling rate; added figures to report

--- Part_II/1_Scripts/Knock_Back_Time_Analysis.py
+++ Part_II/1_Scripts/Knock_Back_Time_Analysis.py
@@ -1,5 +1,5 @@
-# ******************************** Run notes *******************************
-# Script calculates the knock back times for different sets of experiments:
+# ******************************** Run notes ***********************************
+# Script used to compare knock back time for various experiments, specifically:
 # 	INTERIOR:
 # 		Compare 2, 7, 13 (Flow and move SB)
 # 		Compare 6, 8, 14 (Shutdown and move SB)
@@ -8,9 +8,10 @@
 # 		Compare 18, 20, (just initial hit) vs. 22, 24 (just bedroom 1)
 # 		Compare 22 (BD1 and interior) vs. 24 (BD1, BD2, and Interior)		
 # 
-# Times are currently determined using the following criteria:
-# 	t = 0 at the start of water flow (event times still need to be verified from video)
-#	
+# Currently, script set up to generate plots of Bedroom 1 temperature data
+# 	over duration from 6 seconds before start of suppression to 60 seconds after 
+# 	start of suppression
+#
 
 import pandas as pd 
 import os as os
@@ -81,7 +82,6 @@ sensor_groups = np.array([ ['End_Hall_Temps','Middle_Hall_Temps','Victim_1_Temps
 						   ['End_Hall_Temps','Middle_Hall_Temps','Start_Hall_Temps','Victim_1_Temps','Victim_3_Temps','Bedroom_1_Temps'],
 						   ['Bedroom_1_Temps'] ])
 
-
 # Define 20 color pallet using RGB values
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
@@ -111,13 +111,6 @@ for Exp_Set in comparison_sets:
 		# Read in experimental data
 		Exp_Data = pd.read_csv(data_location+File_Name+'.csv')
 
-		# # Set output location for results
-		# output_location = output_location_init + Test_Name + '/'
-
-		# # Create directory if it doesn't already exist
-		# if not os.path.exists(output_location):
-		# 	os.makedirs(output_location)
-
 		# Get house & data speed
 		House = Exp_Des.loc[File_Name, 'House']
 		Speed = Exp_Des.loc[File_Name, 'Speed']
@@ -126,27 +119,23 @@ for Exp_Set in comparison_sets:
 		Events = pd.read_csv(channel_location+'Events/'+Test_Name+'_Events.csv', index_col='Event')
 
 		print('Loaded data and event files for '+Test_Name)
-		print()
 
-		extinguish_time = Events['Time'].iloc[extinguish_event_index]
-		print('Check Times:')
-		print('    Extinguish time = '+str(extinguish_time))
+		extinguish_time = Events['Time'].iloc[extinguish_event_index] 	# event time listed in form h:mm:ss
+		
+		# Uncomment to print times in order to verify they are correct
+		# print('Check Times:')
+		# print('    Extinguish time = '+str(extinguish_time))
 
 		# If statements to determine whether or not data is in high speed and assigning time accordingly based on data csv file
 		if Speed == 'low':
-			# Set time to elapsed time column in experimental data.
-			Time = [datetime.datetime.strptime(t, '%H:%M:%S') for t in Exp_Data['Elapsed Time']]
-			mark_freq = 15
-			try:
+			if Exp_Num == '6' or Exp_Num == '18':	
+				data_per_sec = 0.5		# Data collected every other second 
+				extinguish_idx = Exp_Data[Exp_Data['Elapsed Time']=='0'+extinguish_time].index.tolist()[0] 		# timestamp of form hh:mm:ss				
+			else:
+				data_per_sec = 1 	# Data collected every second
 				extinguish_idx = Exp_Data[Exp_Data['Elapsed Time']==extinguish_time].index.tolist()[0]
-			except IndexError:
-				extinguish_idx = Exp_Data[Exp_Data['Elapsed Time']=='0'+extinguish_time].index.tolist()[0]
-			data_per_sec = 1
 		elif Speed == 'high':
-			# Set time to elapsed time column in experimental data.
-			Time = [datetime.datetime.strptime(t, '%M:%S.%f') for t in Exp_Data['Elapsed Time']]
-			mark_freq = 5
-			data_per_sec = 10
+			data_per_sec = 10 	# Data collected every 0.1 second
 			extinguish_idx = Exp_Data[Exp_Data['Elapsed Time']==extinguish_time[2:]+'.0'].index.tolist()[0]
 		else:
 			print('[ERROR!]: data speed not set properly!')
@@ -154,20 +143,15 @@ for Exp_Set in comparison_sets:
 			exit()
 
 		# Get start/end index for data to plot
-		start_df_idx = extinguish_idx-data_per_sec*5
-		end_df_idx = extinguish_idx+data_per_sec*60 
-		print('    Extinguish index = '+str(extinguish_idx))
-		print('    Extinguish time = '+str(Exp_Data['Elapsed Time'].iloc[extinguish_idx]))
-		print('    Start dataframe at '+str(Exp_Data['Elapsed Time'].iloc[start_df_idx]))
-		print('    End dataframe at '+str(Exp_Data['Elapsed Time'].iloc[end_df_idx]))
-		print()
-
-		# Pull ignition time from events dataframe & adjust time accordingly
-		Ignition = datetime.datetime.strptime(Events.loc['Ignition', 'Time'], '%H:%M:%S')
-		Time = [((t - Ignition).total_seconds())/60 for t in Time]
-
-		# Get end time for given test
-		End_Time = (datetime.datetime.strptime(Events['Time']['End Experiment'], '%H:%M:%S')-datetime.datetime.strptime(Events['Time']['Ignition'], '%H:%M:%S')).total_seconds()/60
+		start_df_idx = int(extinguish_idx-data_per_sec*6)
+		end_df_idx = int(extinguish_idx+data_per_sec*60)
+		
+		# Uncomment to print times in order to verify they are correct
+		# print('    Extinguish time = '+str(Exp_Data['Elapsed Time'].iloc[extinguish_idx]))
+		# print('    Extinguish index = '+str(extinguish_idx))
+		# print('    Start dataframe at '+str(Exp_Data['Elapsed Time'].iloc[start_df_idx]))
+		# print('    End dataframe at '+str(Exp_Data['Elapsed Time'].iloc[end_df_idx]))
+		# print()
 
 		if House == 'a':
 			scalefactor = 'ScaleFactor_A'
@@ -216,9 +200,13 @@ for Exp_Set in comparison_sets:
 		# knock_back_time_df['Exp. '+Exp_Num+' Delta T'] = delta_T_col
 		# knock_back_time_df['Exp. '+Exp_Num+' Knock Back Time'] = knock_back_time_col
 
-		# Modified criteria analysis based on change in T over specified time
 		delta_T_col = []
 		knock_back_time_col = []
+
+		# Set marker frequency based on sampling rate
+		mark_freq = 10*data_per_sec
+
+		# Iterate through sensor groups for each comparison set
 		for group in sensor_groups[set_idx]:
 			# Only consider bedroom 1 temps for now
 			if group != 'Bedroom_1_Temps':
@@ -229,26 +217,25 @@ for Exp_Set in comparison_sets:
 
 			# Make dataframe of channels from 5 seconds before extinguishment to 60 seconds after
 			group_data = Exp_Data[channel_names][start_df_idx:end_df_idx+1]
-			
+
 			# Create figure to plot temperatures
 			fig = plt.figure()
-			# plt.rcParams['axes.prop_cycle'] = (cycler('color',tableau20))
 			plt.rc('axes', color_cycle=tableau20)
 			plot_markers = cycle(markers)
 
 			# Print 'Plotting Chart XX'
-			print ('Plotting ' + group.replace('_',' '))
+			print ('Plotting '+group.replace('_',' ')+' for '+Test_Name)
 			print()
 
-			# Begin cycling through channels
+			# Iterate through sensor group channels
 			for channel in channel_names:
-				# # Skip plot quantity if channel name is blank
-				# if pd.isnull(channel):
-				# 	continue
+				# Skip plot quantity if channel name is blank
+				if pd.isnull(channel):
+					continue
 
-	   			# # Skip excluded channels listed in test description file
-				# if any([substring in channel for substring in Exp_Des['Excluded Channels'][Test_Name].split('|')]):
-				# 	continue
+	   			# Skip excluded channels listed in test description file
+				if any([substring in channel for substring in Exp_Des['Excluded Channels'][File_Name].split('|')]):
+					continue
 
 	            # Set scale factor and offset
 				scale_factor = channel_list[scalefactor][channel]
@@ -256,21 +243,21 @@ for Exp_Set in comparison_sets:
 				
 				# Define channel data and time
 				current_data = group_data[channel]
-				end_time = (len(current_data)/data_per_sec)-5
+				end_time = (len(current_data)/data_per_sec)-6
 				range_spacing = 1./data_per_sec
 
-				time = np.arange(-5,end_time,range_spacing)
+				time = np.arange(-6,end_time,range_spacing)
 				
 				# Set data to include slope and intercept
 				current_data = current_data * scale_factor + offset
 
 				# Plot channel data
 				plt.plot(time, current_data, lw=1.5,
-					marker=next(plot_markers), markevery=int(end_time/mark_freq),
+					marker=next(plot_markers), markevery=int(mark_freq),
 					mew=3, mec='none', ms=7, label=channel_list['Title'][channel])
 
 			# Set y-label to degrees F with LaTeX syntax
-			plt.ylabel('Temperature ($^\circ$F)', fontsize = 32)
+			plt.ylabel('Temperature ($^\circ$F)', fontsize=20)
 
 			# Set secondary y-axis label to degrees C
 			secondary_axis_label = 'Temperature ($^\circ$C)'
@@ -281,13 +268,13 @@ for Exp_Set in comparison_sets:
             # Set axis options, legend, tickmarks, etc.
 			ax1 = plt.gca()
 			handles1, labels1 = ax1.get_legend_handles_labels()
-			plt.xlim([-5, end_time-1])
+			plt.xlim([-6, end_time-1])
 			ax1.xaxis.set_major_locator(plt.MaxNLocator(8))
 			ax1_xlims = ax1.axis()[0:2]
-			plt.xticks(fontsize=28)
-			plt.yticks(fontsize=28)
+			plt.xticks(fontsize=16)
+			plt.yticks(fontsize=16)
 			plt.grid(True)
-			plt.xlabel('Time (sec)', fontsize=32)
+			plt.xlabel('Time (sec)', fontsize=20)
 
 			# Secondary y-axis parameters
 			# ax2 = ax1.twinx()
@@ -298,18 +285,18 @@ for Exp_Set in comparison_sets:
 
 			# Secondary x-axis for event info
 			ax3=ax1.twiny()
-			ax3.set_xlim(-5,end_time)
+			ax3.set_xlim(-6,end_time)
 			plt.axvline(0,color='0',lw=2) 
 			ax3.set_xticks([0])
 			plt.setp(plt.xticks()[1], rotation=60)		
-			ax3.set_xticklabels(['Start Suppression'], fontsize=18, ha='left')
-			fig.set_size_inches(20, 16)
+			ax3.set_xticklabels(['Start Suppression'], fontsize=10, ha='left')
+			fig.set_size_inches(9, 8)
 			plt.tight_layout()
 			
-			plt.legend(handles1, labels1, loc='upper right', fontsize=24, handlelength=3)
+			plt.legend(handles1, labels1, loc='upper right', fontsize=10, handlelength=3)
 
 	        # Save plot to file
-			plt.savefig(output_location+Test_Name+'_'+group+'.png')
+			plt.savefig(output_location+Test_Name+'_'+group+'.pdf')
 			plt.close('all')
 
 			# # Store initial temperatures at start of extinguishment
