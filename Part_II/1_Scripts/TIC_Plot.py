ea1a1d3516d63d092959bb2eb6b11a96b83c3bdc
Robin Zevotek <Robin.Zevotek@ul.com>
2017-10-09 02:42:20 -0400

    add tic plotter

---
+++ Part_II/1_Scripts/TIC_Plot.py
@@ -0,0 +1,15 @@
+#Plot TIC Data from Exp_12
+# tic_data = pd.read_csv('../../../../../Desktop/TIC_data.csv').set_index('Time')
+
+# plt.plot(all_exp_data['Experiment_12_Data']['1TCV7'], label='7ft Temperature')
+# plt.plot(all_exp_data['Experiment_12_Data']['1TCV5'], label='5ft Temperature')
+# plt.plot(all_exp_data['Experiment_12_Data']['1TCV3'], label='3ft Temperature')
+# plt.plot(all_exp_data['Experiment_12_Data']['1TCV1'], label='1ft Temperature')
+# plt.plot(tic_data, label='Thermal Imaging Camera Temperature', color='black', lw=3)
+# plt.legend()
+# plt.xlim([208,264])
+# plt.ylim([0,800])
+# plt.xlabel('Time (Seconds)', fontsize=14)
+# plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
+# plt.savefig('../../../../../Desktop/TIC.png')
+# exit()
