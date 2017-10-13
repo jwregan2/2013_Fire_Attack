7f8777edc6c6b8a92720a0b1d6b3bc57d993e35b
Julie Bryant <julie.bryant@ul.com>
2017-10-13 14:57:44 -0400

    Addition of TIC Graphs

--- Part_II/1_Scripts/TIC_Plot.py
+++ Part_II/1_Scripts/TIC_Plot.py
@@ -5,62 +5,54 @@ import os
 
 data_location = '../2_Data/'
 
+tic_location = '../2_Data/TIC_Data/'
+
 all_exp_data = pickle.load(open(data_location + 'all_exp_data.dict', 'rb'))
 
 output_location = '../0_Images/Script_Figures/Experiment_Analysis/Thermal_Imaging_Camera/'
+
+end_time={'Experiment_1':418, 'Experiment_12':326, 'Experiment_17':324, 'Experiment_19':574}
+end_temp_hall={'Experiment_1':700, 'Experiment_12':700, 'Experiment_17':1700, 'Experiment_19':1200}
+end_temp_door={'Experiment_1':400, 'Experiment_12':400, 'Experiment_17':600, 'Experiment_19':600}
 
 # If the folder doesn't exist create it.
 if not os.path.exists(output_location):
 	os.makedirs(output_location)
 
-# Plot TIC Data from Exp_12
-tic_data = pd.read_csv(data_location + 'TIC_Data/Experiment_12_Data_2.csv').set_index('Time')
+# establish new variable 
+experiments = ['Experiment_1', 'Experiment_12', 'Experiment_17', 'Experiment_19']
 
-plt.plot(all_exp_data['Experiment_12_Data']['1TCV7'], label='7ft Temperature')
-plt.plot(all_exp_data['Experiment_12_Data']['1TCV5'], label='5ft Temperature')
-plt.plot(all_exp_data['Experiment_12_Data']['1TCV3'], label='3ft Temperature')
-plt.plot(all_exp_data['Experiment_12_Data']['1TCV1'], label='1ft Temperature')
-plt.plot(tic_data['Hall_IR'], label='Hall Thermal Imaging Camera Temperature', color='black', lw=3)
-plt.plot(tic_data['Front_Door_IR'], label='Front Door Thermal Imaging Camera Temperature', color='blue', lw=3)
-plt.legend()
-plt.xlim([0,326])
-plt.ylim([0,800])
-plt.xlabel('Time (Seconds)', fontsize=14)
-plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
-plt.savefig('../0_Images/Script_Figures/Experiment_Analysis/Thermal_Imaging_Camera/TIC_12.png')
-plt.close()
+for exp in experiments:
+	
+# Plot TIC Data for Hall IR
+	print (tic_location + exp + '_Data.csv')
+	tic_data = pd.read_csv(tic_location + exp + '_Data.csv').set_index('Time')
 
-# Plot TIC Data from Exp_1
-tic_data = pd.read_csv(data_location + 'TIC_Data/Experiment_1_Data.csv').set_index('Time')
+	plt.plot(all_exp_data[exp + '_Data']['1TCV7'], label='7ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['1TCV5'], label='5ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['1TCV3'], label='3ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['1TCV1'], label='1ft Temperature')
+	plt.plot(tic_data['Hall'], label='Hall Thermal Imaging Camera Temperature', color='black', lw=3)
+	plt.legend()
+	plt.xlim([0,end_time[exp]])
+	plt.ylim([0,end_temp_hall[exp]])
+	plt.xlabel('Time (Seconds)', fontsize=14)
+	plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
+	plt.savefig(output_location + exp + '_Hall.png')
+	plt.close()
 
-plt.plot(all_exp_data['Experiment_1_Data']['1TCV7'], label='7ft Temperature')
-plt.plot(all_exp_data['Experiment_1_Data']['1TCV5'], label='5ft Temperature')
-plt.plot(all_exp_data['Experiment_1_Data']['1TCV3'], label='3ft Temperature')
-plt.plot(all_exp_data['Experiment_1_Data']['1TCV1'], label='1ft Temperature')
-plt.plot(tic_data['Hall_IR'], label='Thermal Imaging Camera Temperature', color='black', lw=3)
-plt.plot(tic_data['Front_Door_IR'], label='Thermal Imaging Camera Temperature', color='blue', lw=3)
-plt.legend()
-plt.xlim([0,418])
-plt.ylim([0,800])
-plt.xlabel('Time (Seconds)', fontsize=14)
-plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
-plt.savefig('../0_Images/Script_Figures/Experiment_Analysis/Thermal_Imaging_Camera/TIC_1.png')
-plt.close()
+# Plot TIC Data for Front Door IR
+	plt.plot(all_exp_data[exp + '_Data']['4TCV7'], label='7ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['4TCV5'], label='5ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['4TCV3'], label='3ft Temperature')
+	plt.plot(all_exp_data[exp + '_Data']['4TCV1'], label='1ft Temperature')
+	plt.plot(tic_data['Front'], label='Front Door Thermal Imaging Camera Temperature', color='black', lw=3)
+	plt.legend()
+	plt.xlim([0,end_time[exp]])
+	plt.ylim([0,end_temp_door[exp]])
+	plt.xlabel('Time (Seconds)', fontsize=14)
+	plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
+	plt.savefig(output_location + exp +'_Door.png')
+	plt.close()
 
-# Plot TIC Data from Exp_17
-tic_data = pd.read_csv(data_location + 'TIC_Data/Experiment_17_Data.csv').set_index('Time')
-
-plt.plot(all_exp_data['Experiment_17_Data']['1TCV7'], label='7ft Temperature')
-plt.plot(all_exp_data['Experiment_17_Data']['1TCV5'], label='5ft Temperature')
-plt.plot(all_exp_data['Experiment_17_Data']['1TCV3'], label='3ft Temperature')
-plt.plot(all_exp_data['Experiment_17_Data']['1TCV1'], label='1ft Temperature')
-plt.plot(tic_data['Hall_IR'], label='Thermal Imaging Camera Temperature', color='black', lw=3)
-plt.plot(tic_data['Front_Door_IR'], label='Thermal Imaging Camera Temperature', color='blue', lw=3)
-plt.legend()
-plt.xlim([0,324])
-plt.ylim([0,1750])
-plt.xlabel('Time (Seconds)', fontsize=14)
-plt.ylabel('Temperature ($^\circ$F)', fontsize=14)
-plt.savefig('../0_Images/Script_Figures/Experiment_Analysis/Thermal_Imaging_Camera/TIC_17.png')
-plt.close()
 exit()
